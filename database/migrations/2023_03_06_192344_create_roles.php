d3326490513ce80d12f643abd78a5ed12fb524cd
leonardo <pintoleonardo1023@gmail.com>
2023-03-09 10:25:04 -0500

    project

---
+++ database/migrations/2023_03_06_192344_create_roles.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $rolAdmin = Role::create(['name'=>'admin']);
+        $rolVista = Role::create(['name'=>'vista']);
+        $user = user::find(1);
+        $user->assignRole($rolAdmin);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
